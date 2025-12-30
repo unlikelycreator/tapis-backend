7d4ed7fd69073d142909090904566a435436eef0
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2025-12-30 22:55:09 +0530

    Set JSON limit to 50mb in express middleware
    Increase JSON payload limit to 50mb

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const esgRoutes = require('./routes/esgReportRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/precincts', precinctRoutes);
 app.use('/api/duns', dunRoutes);
